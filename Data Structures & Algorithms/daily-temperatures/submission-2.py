b6810c684ef2a4d1a2c58e1f75f34b53ccf56613
arygoel42 <aryan_goel@outlook.com>
2026-06-19 22:42:50 -0700

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+
+        # monotonic stack pattern
+        stack = []
+
+        res = [0] * len(temperatures)
+
+
+        
+
+        for i in range(len(temperatures) - 1, -1, -1):
+
+            while stack and temperatures[stack[-1]] <= temperatures[i]: 
+                stack.pop()
+
+            
+            if stack: 
+                res[i] = stack[-1] - i
+
+
+            
+            stack.append(i)
+
+        return res 
+
+            
+
+             
